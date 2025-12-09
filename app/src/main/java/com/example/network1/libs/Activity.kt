525576525f5ce9cf33c7a10e0c1d5cab9d2f45b0
Rodya187 <r.dubanov@vibelab.ru>
2025-12-09 19:07:35 +0300

    fix mistakes

--- app/src/main/java/com/example/network1/libs/Activity.kt
+++ app/src/main/java/com/example/network1/libs/Activity.kt
@@ -17,8 +17,16 @@ import com.example.network1.libs.network.RetrofitClient
 import com.google.gson.Gson
 import com.squareup.moshi.Json
 import com.squareup.moshi.Moshi
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
+import kotlin.text.get
 
 class ActivityProducts : AppCompatActivity() {
 
@@ -90,5 +98,4 @@ val client = HttpClient(Android) {
     }
 }
 
-val products: List<Product> = client.get("https://fakestoreapi.com/products")
-val products: List<Product> = client.get("https://fakestoreapi.com/products")
+//val products: List<Product> = client.get("https://fakestoreapi.com/products") as List<Product>
